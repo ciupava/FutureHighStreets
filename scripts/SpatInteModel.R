205d9d30f0f6639f2bc624613c3d4c0268d062b6
ciupava <ciupava@gmail.com>
2021-08-19 09:12:37 +0100

    Adding data folder

---
+++ scripts/SpatInteModel.R
@@ -0,0 +1,6 @@
+# Title     : SpatInteModel.R
+# Objective : Build a spatial interaction model to analyse the high streets population patterns in UK
+# Created by: azanchetta
+# Created on: 19/08/2021
+
+getwd()
